0063be833a640d1b46eff0421390423cbeddd556
RaphWa <121903709+RaphWa@users.noreply.github.com>
2023-01-04 15:43:05 +0100

    Create main.py

---
+++ meistGesagtImDeutschenBundestag/main.py
@@ -0,0 +1,6 @@
+import os
+
+#compiles and starts the java program
+if __name__ == '__main__':
+    os.system("javac dataProcessing/StartTheProgram.java")
+    os.system("java dataProcessing/StartTheProgram")
